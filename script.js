f72705df4211647eada69ddffb176a94bf504ec7
SNagaAbhilash <nagaabhi83@gmail.com>
2024-10-27 14:08:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("clickMeButton").addEventListener("click", function() {
+    document.getElementById("message").innerText = "Hii,This is a simple server running on docker conatiner!";
+});
